0b00bfb28dcd14c2dea83b8b8df1c804e2ded2a2
Shizun Ge <shizunge@gmail.com>
2021-01-27 15:46:38 -0800

    cleanup function NewUserBatch, no need to set the limit to query

--- api/feed.go
+++ api/feed.go
@@ -59,7 +59,7 @@ func (h *handler) refreshFeed(w http.ResponseWriter, r *http.Request) {
 
 func (h *handler) refreshAllFeeds(w http.ResponseWriter, r *http.Request) {
 	userID := request.UserID(r)
-	jobs, err := h.store.NewUserBatch(userID, h.store.CountFeeds(userID))
+	jobs, err := h.store.NewUserBatch(userID)
 	if err != nil {
 		json.ServerError(w, r, err)
 		return
